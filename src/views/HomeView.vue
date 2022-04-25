8186e0f28a321800884c24ad7c7f786597cb3f42
Leonardo Treviso <leonardosistemas@outlook.com>
2022-04-24 23:13:37 -0300

    feat: criando projeto inicial

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,74 @@
+<script setup lang="ts">
+import {onMounted, reactive} from 'vue';
+import Order from '@/domain/entity/Order';
+import Item from '@/domain/entity/Item';
+
+const { serviceFactory} = defineProps(["serviceFactory"]);
+
+const items : Item[] = reactive([]);
+const itemsIndex: { [idItem: number]: Item } = reactive({});
+const order : Order = reactive(new Order());
+
+
+onMounted(async()=>{
+  const itemsService = serviceFactory.createItemsService();
+  const itemsData = await itemsService.getItems();
+  for(const itemData of itemsData) {
+    items.push(itemData);
+    itemsIndex[itemData.idItem] = itemData;
+  }
+
+})
+
+</script>
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="row">
+    <div class="col-md-8 main">
+    {{itemsIndex}} 
+      <div class="row">
+        <div class="col-md-4" v-for="item in items">
+        <div class="card card-default text-center">
+          <span>{{item.description}}</span>
+          <h5>{{item.formattedPrice}}</h5>
+          <br/>
+          <button class="btn btn-info" @click="order.addItem(item)">Buy</button>
+        </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 order">
+      <h5>Order</h5>
+      <hr/>
+      <div v-for="orderItem in order.orderItems">
+        <div class="row">
+          <div class="col-md-2">
+            {{orderItem.quantity}} 
+          </div>
+          <div class="col-md-6">
+            {{itemsIndex[orderItem.idItem].description}} 
+          </div>
+          <div class="col-md-3">
+            {{orderItem.getPrice()}} 
+          </div>
+        </div>
+      </div>
+      <hr/>
+      <h5>{{order.total}}</h5>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style>
+.main {
+  padding: 20px;
+}
+.card {
+  padding: 20px;
+}
+.order {
+  padding: 20px;
+  background-color: #DDD;
+  min-height: 100vh;
+}
+</style>
