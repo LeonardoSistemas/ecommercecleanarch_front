fe795d2ece2d1dd82cf2625a7174a10d5a62a533
Leonardo Treviso <leonardosistemas@outlook.com>
2022-04-26 23:17:59 -0300

    test: criando test para retirar item da lista

--- tests/unit/Order.spec.ts
+++ tests/unit/Order.spec.ts
@@ -19,7 +19,21 @@ test("Deve criar um pedido com vários itens duplicados e eles devem ser agrupad
   order.addItem(new Item(3,"Cabo", 30, "R$ 30,00"));
   order.addItem(new Item(3,"Cabo", 30, "R$ 30,00"));
   order.addItem(new Item(3,"Cabo", 30, "R$ 30,00"));
-  expect(order.orderItems).toHaveLength(3);
-  expect(order.orderItems[2].quantity).toBe(3);
-  expect(order.orderItems[2].getPrice()).toBe(90);
+  expect(order.orderItem).toHaveLength(3);
+  expect(order.orderItem[2].quantity).toBe(3);
+  expect(order.orderItem[2].getPrice()).toBe(90);
+})
+
+test("Deve criar um pedido, adicionar itens e pagá-los", function () {
+  const order= new Order();
+  order.addItem(new Item(1,"Guitarra", 1000, "R$ 1.000,00"));
+  order.addItem(new Item(2,"Amplificador", 5000, "R$ 5.000,00"));
+  order.addItem(new Item(3,"Cabo", 30, "R$ 30,00"));
+  order.addItem(new Item(3,"Cabo", 30, "R$ 30,00"));
+  order.addItem(new Item(3,"Cabo", 30, "R$ 30,00"));
+  order.deleteItem(order.orderItem[2]);
+  const total = order.getTotal();
+  expect(total).toBe(6060);
+  expect(order.orderItem[2].quantity).toBe(2);
+  expect(order.orderItem[2].getPrice()).toBe(60);
 })
