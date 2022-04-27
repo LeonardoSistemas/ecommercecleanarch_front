95c9fc5fd60ab4c5e6c307a81ad5fd6ddb4ccd6b
Leonardo Treviso <leonardosistemas@outlook.com>
2022-04-26 23:17:41 -0300

    feat: criando o método para retirar item da lista

--- src/domain/entity/Order.ts
+++ src/domain/entity/Order.ts
@@ -1,26 +1,41 @@
 import Item from "./Item";
-import OrderItems from "./OrderItem";
+import OrderItem from "./OrderItem";
 
 export default class Order {
-    orderItems : OrderItems [];
-    total : number;
+    orderItem: OrderItem[];
+    total: number;
+    cpf?: string;
+    code?: string;
 
     constructor() {
-        this.orderItems = [];
+        this.orderItem = [];
         this.total = 0;
+        this.cpf = "935.411.347-80";
     }
 
-    addItem (item : Item) {
-        const orderItem = this.orderItems.find(orderItem => orderItem.idItem === item.idItem);
-        if(orderItem){
-          orderItem.quantity ++;
-        }else{
-        this.orderItems.push(new OrderItems(item.idItem, 1, item.price));
+    addItem(item: Item) {
+        const orderItem = this.orderItem.find(orderItem => orderItem.idItem === item.idItem);
+        if (orderItem) {
+            orderItem.quantity++;
+        } else {
+            this.orderItem.push(new OrderItem(item.idItem, 1, item.price));
         }
         this.total += item.price;
     }
 
-    getTotal () {
+    getTotal() {
         return this.total;
+    }
+
+    deleteItem(orderItem: OrderItem) {
+        orderItem.quantity--;
+        this.total -= orderItem.itemPrice;
+        if (orderItem.quantity === 0) {
+            this.orderItem.splice(this.orderItem.indexOf(orderItem), 1);
+        }
+    }
+
+    setCode(code: string) {
+        this.code = code;
     }
 }
