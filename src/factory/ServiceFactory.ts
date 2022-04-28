40e67befc8b283be68dcf386bba20bda2b61d759
Leonardo Treviso <leonardosistemas@outlook.com>
2022-04-27 22:43:28 -0300

    feat: criando classes de coupon

--- src/factory/ServiceFactory.ts
+++ src/factory/ServiceFactory.ts
@@ -1,16 +1,22 @@
 import HttpClient from "@/infra/HttpClient";
+import CouponsService from "@/service/CouponsService";
 import ItemsService from "@/service/ItemsService";
-import OrdersService from "@/service/OrdersServices";
+import OrdersService from "@/service/OrdersService";
 
 export default class ServiceFactory {
 
-    constructor(readonly httpClient : HttpClient, readonly baseUrl : string){}
+	constructor (readonly httpClient: HttpClient, readonly baseUrl: string) {
+	}
 
-    createItemsService() : ItemsService {
-        return new ItemsService(this.httpClient, this.baseUrl);
-    }
+	createItemsService(): ItemsService {
+		return new ItemsService(this.httpClient, this.baseUrl);
+	}
 
-    createOrdersService() : OrdersService {
-        return new OrdersService(this.httpClient, this.baseUrl);
-    }
+	createOrdersService(): OrdersService {
+		return new OrdersService(this.httpClient, this.baseUrl);
+	}
+
+	createCouponsService(): CouponsService {
+		return new CouponsService(this.httpClient, this.baseUrl);
+	}
 }
